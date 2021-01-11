9af091e55974ea5cad917991c268cd7f7cbc20be
theodore cleveland <theodore.cleveland@att.net>
2021-01-11 14:49:52 -0600

    update

--- 1-Lessons/Lesson04/ScriptsInLecture/VariableSpaceWSP.R
+++ 1-Lessons/Lesson04/ScriptsInLecture/VariableSpaceWSP.R
@@ -43,9 +43,13 @@ bse<-numeric(0) # numeric vector for bottom elevations
 wse<-numeric(0) # numeric vector for water surface elevations
 delta_depth<-(begin_depth-end_depth)/(how_many)  # change in depth for finding spatial steps
 depth[1]<-begin_depth # assign downstream value
-for (i in 2:how_many){depth[i]<-depth[1]-i*delta_depth} # uniform depths
+for (i in 2:how_many){
+  depth[i]<-depth[1]-i*delta_depth# uniform depths
+  print(depth[i])
+  } 
 velocity<-discharge/area(depth,width) # velocity for each depth
 deltax<-numeric(0) # numeric vector for spatial steps
+#print(deltax)
 # next for loop is very FORTRANesque!  
 for (i in 1:(how_many-1)){
     depth_bar<-avg2point(depth[i],depth[i+1]); #compute average depth in reach
@@ -53,8 +57,12 @@ for (i in 1:(how_many-1)){
     perimeter_bar<-perimeter(depth_bar,width); #compute average wetted perimeter
     radius_bar<-radius(area_bar,perimeter_bar); #compute average hydraulic radius
     friction<-slope_f(discharge,mannings_n,area_bar,radius_bar) #compute friction slope
+    print((depth[i]))# + (velocity[i]^2)/(2*9.8)))
+    print((depth[i+1]))#+(velocity[i+1]^1)/(1)))
     deltax[i]<-( (depth[i+1]+(velocity[i+1]^2)/(2*9.8))	- (depth[i] + (velocity[i]^2)/(2*9.8)) )/(slope-friction); # compute change in distance for each change in depth
-}
+ #   print(friction)
+#    print(deltax[i])
+    }
 distance<-numeric(0); # space for computing cumulative distances
 distance[1]<-0;
 bse[1]<-0; # bottom elevation at origin
@@ -67,9 +75,10 @@ wse<-bse+depth # water surface elevations
 plot(distance,depth,col="blue",type="l",lwd=8,xlim=c(0,max(distance)),ylim=c(min(bse),max(wse)));
 lines(distance,bse,type="l",col="grey",lwd=6);
 z<- cbind(distance,depth,bse,wse) # bind output into 4 columns
+print(delta_depth)
 return(z)
 #
 }
 # Textbook Example
-# backwater(begin_depth=8,end_depth=5,how_many=31,discharge=55.4,width=5,mannings_n=0.02,slope=0.001)
-zz<-backwater(begin_depth=0.1,end_depth=0.47,how_many=20,discharge=1,width=51,mannings_n=0.01,slope=0.000)
+zz <- backwater(begin_depth=8,end_depth=5,how_many=31,discharge=55.4,width=5,mannings_n=0.02,slope=0.001)
+##zz<-backwater(begin_depth=0.1,end_depth=0.47,how_many=20,discharge=1,width=51,mannings_n=0.01,slope=0.000)
